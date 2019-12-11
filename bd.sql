56cdfa88ac4edd9a420124b73c8f5a0fdef5a47f
xxmanuelxx23 <55803730+xxmanuelxx23@users.noreply.github.com>
2019-12-11 11:43:33 -0500

    Base-De-Datos-4.8

---
+++ bd.sql
@@ -0,0 +1,11 @@
+create table maquina (
+        codigo int,
+        marca varchar(200),
+        antiguedad varchar(100)
+); 
+insert into maquina (codigo,marca,antiguedad) values 
+(1,'Excavadora','123'),
+(2,'Volqueta','124'),
+(3,'Pavimentadora','125'),
+(4,'Retroexcavadora','126'),
+(5,'Pavimentadora','127');
